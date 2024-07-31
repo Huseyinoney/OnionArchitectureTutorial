8062be7a49144a315328f8231d891847a9732386
Huseyinoney <hnsn1951@gmail.com>
2024-07-31 16:02:28 +0300

    Add project files.

---
+++ Core/OnionArchitecture.Domain/Entities/Event.cs
@@ -0,0 +1,23 @@
+﻿using OnionArchitecture.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Domain.Entities
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        
+        public DateTimeOffset CreatedDate { get; set; }
+
+        public DateTimeOffset UpdatedDate { get; set; }
+        public string Title { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+
+        public Location Location { get; set; }
+    }
+}
